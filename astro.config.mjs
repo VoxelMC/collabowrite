d823cfdd7d12235f0404e12bf6b95030c55ae797
voxel!() <voxelmc@hotmail.com>
2024-02-21 16:20:50 -0800

    switch to vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,13 @@ import tailwind from '@astrojs/tailwind';
 // import preact from '@astrojs/preact';
 
 import qwikdev from '@qwikdev/astro';
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), /* svelte(), preact(), */qwikdev()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
