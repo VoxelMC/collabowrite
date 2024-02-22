6e7650e0e646e35f946c2c9b8232d7c82f778337
voxel!() <voxelmc@hotmail.com>
2024-02-21 19:44:02 -0800

    forgot to change it back to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,16 @@ import tailwind from '@astrojs/tailwind';
 
 import qwikdev from '@qwikdev/astro';
 import node from '@astrojs/node';
-
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), qwikdev()],
 	output: 'server',
-	// adapter: vercel(),
-	adapter: node({ mode: 'standalone' }),
+	adapter: vercel(),
+	// adapter: node({ mode: 'standalone' }),
+	vite: {
+		plugins: [nodeResolve()],
+	},
 });
