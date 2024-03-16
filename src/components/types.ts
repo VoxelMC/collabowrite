fcc57ae8f4b20890eb0b337d1fe95430bc6aa30e
voxel!() <voxelmc@hotmail.com>
2024-03-15 17:16:13 -0700

    Add dashboard page

---
+++ src/components/types.ts
@@ -0,0 +1,11 @@
+export interface LabData {
+	id: number;
+	uuid: string;
+	owner: string;
+	data: string;
+	last_edited: string;
+	created_at: string;
+	is_private: boolean;
+	description: string;
+	name: string;
+}
